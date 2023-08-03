8c6eeedd2080aae54a0acd8e67f43f4a9f2eeccd
May <maykhine.ada@gmail.com>
2023-08-03 14:49:45 -0400

    debugging on insert booking date issue

--- src/assets/Calander.tsx
+++ src/assets/Calander.tsx
@@ -11,12 +11,28 @@ export type CalanderProps = {
 };
 
 export const Calander: FC<CalanderProps> = (props) => {
-  const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([
-    null,
-    null,
-  ]);
+  const [dateRange, setDateRange] = //useState([]);
+    useState<[Date | null, Date | null]>([null, null]);
 
   const navigate = useNavigate();
+
+  const [bookedDatesDic, setBookedDatesDic] = useState({});
+
+  const insertBookingDays = (startDate: DateTime, endDate: DateTime) => {
+    // 86400 sec or 1 days short
+    if (endDate - startDate == 0) {
+      console.log("Same day trip");
+      setBookedDatesDic({ startDate: 1 });
+    } else {
+      const days = (endDate - startDate) / (86400 * 1000);
+      for (let i = 0; i <= days; i++) {
+        const iDate = startDate.plus({ days: i });
+        // console.log("date: ", startDate.plus({ days: i }));
+        setBookedDatesDic({ ...bookedDatesDic, iDate: 1 });
+      }
+      console.log("bookedDatesDIc: ", bookedDatesDic);
+    }
+  };
 
   return (
     <>
@@ -28,36 +44,18 @@ export const Calander: FC<CalanderProps> = (props) => {
           allowDeselect
           value={dateRange}
           onChange={setDateRange}
-          // getDayProps={(date) => {
-          //   props.bookDateRange.forEach((dateRange: Array<Date>) => {});
-          //   if (date >= dateRange[0] && date <= dateRange[1]) {
-          //     console.log("Date found");
-          //     console.log("Date: ", date, dateRange[0], dateRange[1]);
-          //   }
-          // }}
-
           getDayProps={(dateJs) => {
             const dt = DateTime.fromJSDate(dateJs);
 
-            // const dateStrToBookingCount = {
-            //   "08/01/2023": 1,
-            //   "08/02/2023": 1,
-            //   "08/03/2023": 1,
-            //   "08/04/2023": 1,
-            //   "08/05/2023": 1,
-            //   "08/06/2023": 1,
-            //   "08/07/2023": 1,
-            // }
             props.bookDateRange.forEach((dateRange) => {
-              console.log(
-                "Date time happending? dt: ",
-                dt,
-                "dateRange 0: ",
-                dateRange[0],
-                "date range 1: ",
-                DateTime.fromJSDate(dateRange[1])
-              );
-
+              // console.log(
+              //   "Date time happending?? dt: ",
+              //   dt,
+              //   "dateRange 0: ",
+              //   DateTime.fromHTTP(dateRange[0]),
+              //   "date range 1: ",
+              //   DateTime.fromJSDate(dateRange[1])
+              // );
               // if (
               //   dt >= DateTime.fromJSDate(dateRange[0]) &&
               //   dt <= DateTime.fromJSDate(dateRange[1])
@@ -69,11 +67,34 @@ export const Calander: FC<CalanderProps> = (props) => {
         ></DatePicker>
         <Button
           onClick={() => {
-            props.setBookDateRange([...props.bookDateRange, [dateRange]]);
-            navigate("/");
+            if (dateRange[0] == null) {
+              console.log("Start date is NULL");
+              return;
+            }
+            calculateBookingDays(
+              DateTime.fromJSDate(dateRange[0]),
+              dateRange[1]
+                ? DateTime.fromJSDate(dateRange[1])
+                : DateTime.fromJSDate(dateRange[0])
+            );
+
+            // props.setBookDateRange([...props.bookDateRange, [dateRange]]);
+            // setDateRange([null, null]);
+            // navigate("/");
           }}
         >
           Book
+        </Button>
+        <Button
+          onClick={() => {
+            console.log("bookedDatesDIc: ", bookedDatesDic);
+
+            // console.log("Date range: ", dateRange);
+
+            // console.log("Date range: ", props.bookDateRange);
+          }}
+        >
+          Print the booked Date range
         </Button>
       </Group>
     </>
